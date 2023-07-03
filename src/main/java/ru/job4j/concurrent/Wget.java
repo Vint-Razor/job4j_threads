4d722ee56d2d78c53437c0492dd629baafe5bbbd
Рустам <vint-razor@yandex.ru>
2023-07-03 16:56:42 +0500

    8. Скачивание файла с ограничением. [#144271 # [#144271]]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -3,13 +3,6 @@
  * Чтобы ограничить скорость скачивания, нужно засечь время скачивания 1024 байт.
  * Если время меньше указанного, то нужно выставить паузу за счет Thread.sleep.
  * Пауза должна вычисляться, а не быть константой.
- *
- * Алгоритм:
- * 1. замерить скорость System.nanoTime();
- * 1.1 скачиваем указанное количество байт (байт/сек)
- * 1.2 смотрим, осталось ли ещё время до секунды
- * 2. если осталось то интегрируем sleep
- * 3. sleep выставляем равным оставшемуся времени
  */
 
 package ru.job4j.concurrent;
@@ -22,9 +15,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Wget implements Runnable {
-    private static final String FILE = "file2.xml";
+    private static final String FILE = "file.xml";
     private static final long SEC = 1000L;
-
     private final String url;
     private final int speed;
 
@@ -38,12 +30,11 @@ public class Wget implements Runnable {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
              FileOutputStream fileOutputStream = new FileOutputStream(FILE)) {
             byte[] dataBuffer = new byte[speed];
-            int byteRead = 0;
-            long start = 0;
-            long end = 0;
-            while (byteRead != -1) {
+            int byteRead;
+            long start;
+            long end;
+            while ((byteRead = in.read(dataBuffer, 0, speed)) != -1) {
                 start = System.nanoTime();
-                byteRead = in.read(dataBuffer, 0, speed);
                 end = System.nanoTime();
                 long difference = end - start;
                 if (difference < SEC) {
@@ -56,9 +47,9 @@ public class Wget implements Runnable {
                 fileOutputStream.write(dataBuffer, 0, byteRead);
             }
         } catch (MalformedURLException e) {
-            throw new IllegalArgumentException(String.format("неправильно указан url-адрес %s", url));
+            throw new IllegalArgumentException(String.format("не правильный формат url-адреса %s", url));
         } catch (FileNotFoundException e) {
-            throw new IllegalArgumentException(String.format("файл не найден %s", FILE));
+            throw new IllegalArgumentException(String.format("url не найден, либо отсутствует соединение %s", FILE));
         } catch (IOException e) {
             e.printStackTrace();
         }

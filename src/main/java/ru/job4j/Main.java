9388e40e4417c6020e344ea0bddaf2d666fe33d9
Vint-Razor <vint-razor@yandex.ru>
2023-09-17 22:31:51 +0500

    3.4. Временное хранение. git stash. [#504832]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -4,9 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(printTest());
+        System.out.println(printWord());
     }
 
     public static String printTest() {
         return "Test";
+    }
+
+    public static String printWord() {
+        return "Word";
     }
 }
